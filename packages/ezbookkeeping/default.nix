eb4128e0f25546872cc3a8708587f01cfa2e2379
Adithya Nair <adtya@adtya.xyz>
2025-02-02 17:10:56 +0530

    bump ezbookkeeping to master

--- packages/ezbookkeeping/default.nix
+++ packages/ezbookkeeping/default.nix
@@ -1,11 +1,11 @@
 { lib, fetchFromGitHub, buildGoModule, buildNpmPackage, git }:
 let
-  version = "0.7.0";
+  version = "unstable-20250202";
   src = fetchFromGitHub {
     owner = "mayswind";
     repo = "ezbookkeeping";
-    rev = "a26397131d629e214a493c75e64b88e08ad6704d";
-    hash = "sha256-vj97QKPcAqNyS816KHMxIcwIizs4c6TRjMHTyqERu6A=";
+    rev = "a5e7c483ef6de7bad51b47498953a6a553ded3e1";
+    hash = "sha256-QhyhVr6dL1XV6mfcFDTA2/UtUi/EO3QOnzUbjBYGqjU=";
   };
   _meta = {
     description = "A lightweight personal bookkeeping app hosted by yourself";
@@ -19,20 +19,26 @@ in
     pname = "ezbookkeeping-frontend";
     inherit src version;
 
-    npmDepsHash = "sha256-f2cZ8XNhcK0Sh3qVNRMFcqRPalVNduCRPTSAtkL48/A=";
+    npmDepsHash = "sha256-MxXAL+r3Kvh62Fpb3/8ud5GaG22SOOam+nDeIsDzkUo=";
     GITCOMMIT = src.rev;
     patches = [ ./git-rev-sync.patch ];
 
     installPhase = ''
+      runHook preInstall
+
       cp -r ./dist $out
+
+      runHook postInstall
     '';
+
     meta = _meta;
   };
+
   backend = buildGoModule rec {
     pname = "ezbookkeeping-backend";
     inherit src version;
 
-    vendorHash = "sha256-ENQggA9DaCn5o3KG5DoT1iK0NTOGQH4H0SkG4ClDFCQ=";
+    vendorHash = "sha256-3ynXWGpkej7AQZToDze7MT6FzpZjyaTREy8pbjCUzto";
 
     ldflags = [
       "-s"
@@ -43,6 +49,11 @@ in
     ];
 
     checkPhase = false;
+
+    postInstall = ''
+      cp -r ${src}/templates $out/bin/templates
+    '';
+
     meta = _meta // {
       mainProgram = "ezbookkeeping";
     };
